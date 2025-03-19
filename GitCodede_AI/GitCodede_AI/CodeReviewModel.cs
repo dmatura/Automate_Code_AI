0d05e54d041fc35090870eeb764ffd9fe10a7a6c
STRYKERCORP\DMatura <digvijay.matura@stryker.com>
2025-03-19 10:06:19 -0500

    Added Tokenizer interface and class for tokenization fo the prompt

---
+++ GitCodede_AI/GitCodede_AI/CodeReviewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodede_AI
+{
+    internal class CodeReviewModel : IAIModelBase
+    {
+        public CodeReviewModel()
+        {
+        }
+
+        public virtual void GetModel()
+        {
+           
+        }
+
+        public virtual void LoadModel(string bitstreamPathOnxx)
+        {
+           
+        }
+
+        public virtual void SaveModel(string destinationPathOnxx)
+        {
+            
+        }
+    }
+ 
+}
