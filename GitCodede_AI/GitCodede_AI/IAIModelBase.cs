8a386b47873c8582dd1de5f910ddb90bd9dded10
STRYKERCORP\DMatura <digvijay.matura@stryker.com>
2025-03-19 09:58:32 -0500

    Initial Model Infra Structure 1) Model Base Class was added 2) Model class concreate added

---
+++ GitCodede_AI/GitCodede_AI/IAIModelBase.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodede_AI
+{
+    internal class IAIModelBase
+    {
+        public IAIModelBase()
+        {
+
+        }
+        public virtual void GetModel()
+        {
+
+        }
+
+        public virtual void LoadModel(string bitstreamPathOnxx)
+        {
+        }
+
+        public virtual void SaveModel(string destinationPathOnxx)
+        {
+        }
+    }
+   
+}
+
---
+++ GitCodede_AI/GitCodede_AI/Model.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GitCodede_AI
+{
+
+    internal class IAIModelBase<T> : IAIModelBase
+    {
+        public IAIModelBase()
+        {
+        }
+
+
+
+    }
+
+    internal class IAIModelBase<T, T1> : IAIModelBase<T>
+    {
+        public IAIModelBase()
+        {
+
+        }
+    }
+
+    internal class AIModelLLMCodeReview : IAIModelBase
+    {
+
+    }
+}
